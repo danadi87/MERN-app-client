087ddef674821586e6c1aedb58aa98b50c7a8c0d
Mirela Dragulescu <dragulescumirela@gmail.com>
2025-02-08 15:40:17 +0100

    cart context and favorites context

--- src/context/favorites.context.jsx
+++ src/context/favorites.context.jsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { AuthContext } from "./auth.context";
 
 const FavoritesContext = createContext();
 
@@ -7,35 +8,50 @@ const API_URL = "http://localhost:5005";
 
 export const FavoritesProviderWrapper = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
+  const { storedToken } = useContext(AuthContext);
 
   const addFavorite = (product) => {
-    axios
-      .post(`${API_URL}/auth/favorites`, product)
-      .then((response) => {
-        setFavorites((prevFavorites) => [...prevFavorites, response.data]);
-      })
-      .catch((error) => console.log(error));
+    if (storedToken) {
+      axios
+        .post(`${API_URL}/auth/favorites`, product, {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        })
+        .then((response) => {
+          setFavorites((prevFavorites) => [...prevFavorites, response.data]);
+        })
+        .catch((error) => console.log(error));
+    }
   };
 
   const removeFavorite = (productId) => {
-    axios
-      .delete(`${API_URL}/auth/favorites/${productId}`)
-      .then((response) => {
-        console.log(response.data);
-        const newFavorites = favorites.filter((product) => {
-          if (productId != product.id) return true;
-        });
-        setFavorites(newFavorites);
-      })
-      .catch((error) => console.log(error));
+    if (storedToken) {
+      axios
+        .delete(`${API_URL}/auth/favorites/${productId}`, {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          const newFavorites = favorites.filter((product) => {
+            if (productId != product.id) return true;
+          });
+          setFavorites(newFavorites);
+        })
+        .catch((error) => console.log(error));
+    }
   };
 
   useEffect(() => {
-    axios
-      .get(`${API_URL}/auth/favorites`)
-      .then((response) => setFavorites(response.data))
-      .catch((error) => console.log(error));
-  }, []);
+    if (storedToken) {
+      axios
+        .get(`${API_URL}/auth/favorites`)
+        .then((response) => setFavorites(response.data))
+        .catch((error) => console.log(error));
+    }
+  }, [storedToken]);
 
   return (
     <FavoritesContext.Provider
--- src/context/shoppingCart.context.jsx
+++ src/context/shoppingCart.context.jsx
@@ -8,34 +8,49 @@ const API_URL = "http://localhost:5005";
 
 export const ShoppingCartProviderWrapper = ({ children }) => {
   const [cart, setCart] = useState([]);
+  const { storedToken } = useContext(AuthContext);
 
   const addToCart = (product) => {
-    axios
-      .post(`${API_URL}/auth/cart`, product)
-      .then((response) => {
-        setCart((prevCart) => [...prevCart, response.data]);
-      })
-      .catch((error) => console.log(error));
+    if (storedToken) {
+      axios
+        .post(`${API_URL}/auth/cart`, product, {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        })
+        .then((response) => {
+          setCart((prevCart) => [...prevCart, response.data]);
+        })
+        .catch((error) => console.log(error));
+    }
   };
 
   const removeCart = (productId) => {
-    axios
-      .delete(`${API_URL}/auth/cart/${productId}`)
-      .then((response) => {
-        console.log(response.data);
-        const newCart = cart.filter((product) => {
-          if (productId != product.id) return true;
-        });
-        setCart(newCart);
-      })
-      .catch((error) => console.log(error));
+    if (storedToken) {
+      axios
+        .delete(`${API_URL}/auth/cart/${productId}`, {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          const newCart = cart.filter((product) => {
+            if (productId != product.id) return true;
+          });
+          setCart(newCart);
+        })
+        .catch((error) => console.log(error));
+    }
   };
 
   useEffect(() => {
-    axios
-      .get(`${API_URL}/auth/cart`)
-      .then((response) => setCart(response.data))
-      .catch((error) => console.log(error));
+    if (storedToken) {
+      axios
+        .get(`${API_URL}/auth/cart`)
+        .then((response) => setCart(response.data))
+        .catch((error) => console.log(error));
+    }
   }, []);
 
   return (
