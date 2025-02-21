200ef1dbb9b2202e1ad33a6d1098c3cd37ff15fa
Mirela Dragulescu <dragulescumirela@gmail.com>
2025-02-21 22:53:51 +0100

    backbutton

--- src/components/ProductAdmin.jsx
+++ src/components/ProductAdmin.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/ProductAdmin.css";
+import { BackButton } from "./BackButton";
+
 export function ProductAdmin() {
   return (
     <div className="product-admin-container">
